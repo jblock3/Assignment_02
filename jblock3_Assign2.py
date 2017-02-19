34a3ca2617b3d1a87bd4c2421469d8977518b1ab
jblock3 <block.jamie123@gmail.com>
2017-02-19 02:47:40 -0500

    First commit

---
+++ jblock3_Assign2.py
@@ -0,0 +1,6 @@
+##
+# This program was coded by Jamie Block (Student number: 250777666)
+# The program is designed to calculate the volume of a cube, a pyramid, or an ellipsoid
+# as many times as he/she pleases.
+# The sentinel value to end the loop and display the final output is "quit".
+#
