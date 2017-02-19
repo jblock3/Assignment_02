d65510e2331f32d297ddc65b4832541e9b19771d
jblock3 <block.jamie123@gmail.com>
2017-02-19 03:10:08 -0500

    Beginnings

--- jblock3_Assign2.py
+++ jblock3_Assign2.py
@@ -4,3 +4,40 @@
 # as many times as he/she pleases.
 # The sentinel value to end the loop and display the final output is "quit".
 #
+
+
+# Initialization of empty lists
+
+cubeVolumeList = []
+pyramidVolumeList = []
+ellipsoidVolumeList = []
+
+# Creating the main function to be called at the end of the program
+
+def main() :
+    shapeInput = (input('Please enter "C" to calculate the volume of a cube, "P" for the volume of a pyramid, "E" for the volume of an ellipsoid, or "quit" to quit: ')).upper()
+    valid = False
+    while not valid :
+        if shapeInput == 'C' :
+            cubeVolume = cubeVolume()
+            print(cubeVolume)
+            cubeVolumeList.append(cubeVolume)
+            shapeInput = (input('Please enter "C" to calculate the volume of a cube, "P" for the volume of a pyramid, "E" for the volume of an ellipsoid, or "quit" to quit: ')).upper()
+        elif shapeInput == 'P' :
+            pyramidVolume = pyramidVolume()
+            print(pyramidVolume)
+            pyramidVolumeList.append(pyramidVolume)
+            shapeInput = (input('Please enter "C" to calculate the volume of a cube, "P" for the volume of a pyramid, "E" for the volume of an ellipsoid, or "quit" to quit: ')).upper()
+        elif shapeInput == 'E' :
+            ellipsoidVolume = ellipsoidVolume()
+            print(ellipsoidVolume)
+            ellipsoidVolumeList.append(ellipsoidVolume)
+            shapeInput = (input('Please enter "C" to calculate the volume of a cube, "P" for the volume of a pyramid, "E" for the volume of an ellipsoid, or "quit" to quit: ')).upper()
+        elif shapeInput == 'QUIT' :
+            valid = True
+        else :
+            print('\nInvalid input! Please try again.')
+            print()
+            shapeInput = (input('Please enter "C" to calculate the volume of a cube, "P" for the volume of a pyramid, "E" for the volume of an ellipsoid, or "quit" to quit: ')).upper()
+    if len(cubeVolumeList) != 0 or len(pyramidVolumeList) != 0 or len(ellipsoidVolumeList) != 0 :
+        
