ae2a55b01c89ab68fa471daa9bf0d23180cb4326
jblock3 <block.jamie123@gmail.com>
2017-02-19 03:41:27 -0500

    Need to fix problems

--- jblock3_Assign2.py
+++ jblock3_Assign2.py
@@ -5,6 +5,26 @@
 # The sentinel value to end the loop and display the final output is "quit".
 #
 
+# Defining functions for calculating the volumes of the shapes
+
+def cubeVolume() :
+    sideLength = float(input('What is the length of one side of the cube?: '))
+    volume = sideLength ** 3
+    print('The volume of a pyramid with a side length of %f is %.1f' % (sideLength, volume))
+
+def pyramidVolume() :
+    base = float(input('What is the base length of the pyramid?: '))
+    height = float(input('What is the height of the pyramid?: '))
+    volume = (1 / 3) * (base ** 2) * height
+    print('The volume of a pyramid with a base of %f and a height of %f is %.1f' % (base, height, volume))
+
+def ellipsoidVolume() :
+    from math import pi
+    r1 = float(input('What is the value of radius 1?: '))
+    r2 = float(input('What is the value of radius 2?: '))
+    r3 = float(input('What is the value of radius 3?: '))
+    volume = (4 / 3) * pi * r1 * r2 * r3
+    print('The volume of an ellipsoid with a radius 1 of %f, a radius 2 of %f, and a radius 3 of %f is %.1f')
 
 # Initialization of empty lists
 
@@ -19,8 +39,7 @@ def main() :
     valid = False
     while not valid :
         if shapeInput == 'C' :
-            cubeVolume = cubeVolume()
-            print(cubeVolume)
+            cubeVolume()
             cubeVolumeList.append(cubeVolume)
             shapeInput = (input('Please enter "C" to calculate the volume of a cube, "P" for the volume of a pyramid, "E" for the volume of an ellipsoid, or "quit" to quit: ')).upper()
         elif shapeInput == 'P' :
@@ -46,31 +65,12 @@ def main() :
         print()
         print('You have come to the end of the session.')
         print('The volumes calculated for each shape are shown below')
-        print('Cube:', cubeVolumeList)
+        for i in range(len(cubeVolumeList)) :
+        print('Cube: {}'.format(cubeVolumeList))
         print('Pyramid:', pyramidVolumeList)
         print('Ellipsoid:', ellipsoidVolumeList)
     else :
-        print('You have come to the end of the session.')
+        print('\nYou have come to the end of the session.')
         print('You did not perform any volume calculations')
 
-# Defining functions for calculating the volumes of the shapes
-
-def cubeVolume() :
-    sideLength = float(input('What is the length of one side of the cube?: '))
-    volume = sideLength ** 3
-    print('The volume of a pyramid with a side length of %f is %.1f' % (sideLength, volume))
-
-def pyramidVolume() :
-    base = float(input('What is the base length of the pyramid?: '))
-    height = float(input('What is the height of the pyramid?: '))
-    volume = (1 / 3) * (base ** 2) * height
-    print('The volume of a pyramid with a base of %f and a height of %f is %.1f' % (base, height, volume))
-
-def ellipsoidVolume() :
-    from math import pi
-    r1 = float(input('What is the value of radius 1?: '))
-    r2 = float(input('What is the value of radius 2?: '))
-    r3 = float(input('What is the value of radius 3?: '))
-    volume = (4 / 3) * pi * r1 * r2 * r3
-    print('The volume of an ellipsoid with a radius 1 of %f, a radius 2 of %f, and a radius 3 of %f is %.1f')
 main()
