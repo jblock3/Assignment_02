00131a6e531913002ffb22ffca5e2785c7d4e1a6
jblock3 <block.jamie123@gmail.com>
2017-02-19 03:17:12 -0500

    Finished def main() fuction

--- jblock3_Assign2.py
+++ jblock3_Assign2.py
@@ -40,4 +40,17 @@ def main() :
             print()
             shapeInput = (input('Please enter "C" to calculate the volume of a cube, "P" for the volume of a pyramid, "E" for the volume of an ellipsoid, or "quit" to quit: ')).upper()
     if len(cubeVolumeList) != 0 or len(pyramidVolumeList) != 0 or len(ellipsoidVolumeList) != 0 :
-        
+        cubeVolumeList.sort()
+        pyramidVolumeList.sort()
+        ellipsoidVolumeList.sort()
+        print()
+        print('You have come to the end of the session.')
+        print('The volumes calculated for each shape are shown below')
+        print('Cube:', cubeVolumeList)
+        print('Pyramid:', pyramidVolumeList)
+        print('Ellipsoid:', ellipsoidVolumeList)
+    else :
+        print('You have come to the end of the session.')
+        print('You did not perform any volume calculations')
+
+main()
