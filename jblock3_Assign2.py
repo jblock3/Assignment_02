532b813e182cb59279172b89d976ee99b280da7b
jblock3 <block.jamie123@gmail.com>
2017-02-20 00:08:08 -0500

    Changed location of appends

--- jblock3_Assign2.py
+++ jblock3_Assign2.py
@@ -10,12 +10,16 @@
 def cubeVolume() :
     sideLength = float(input('What is the length of one side of the cube?: '))
     volume = sideLength ** 3
+    cubeVolumeList.append(volume)
+    cubeVolumeList.sort()
     print('The volume of a pyramid with a side length of %f is %.1f' % (sideLength, volume))
 
 def pyramidVolume() :
     base = float(input('What is the base length of the pyramid?: '))
     height = float(input('What is the height of the pyramid?: '))
     volume = (1 / 3) * (base ** 2) * height
+    pyramidVolumeList.append(pyramidVolume)
+    pyramidVolumeList.sort()
     print('The volume of a pyramid with a base of %f and a height of %f is %.1f' % (base, height, volume))
 
 def ellipsoidVolume() :
@@ -24,6 +28,8 @@ def ellipsoidVolume() :
     r2 = float(input('What is the value of radius 2?: '))
     r3 = float(input('What is the value of radius 3?: '))
     volume = (4 / 3) * pi * r1 * r2 * r3
+    ellipsoidVolumeList.append(ellipsoidVolume)
+    ellipsoidVolumeList.sort()
     print('The volume of an ellipsoid with a radius 1 of %f, a radius 2 of %f, and a radius 3 of %f is %.1f')
 
 # Initialization of empty lists
@@ -40,17 +46,12 @@ def main() :
     while not valid :
         if shapeInput == 'C' :
             cubeVolume()
-            cubeVolumeList.append(cubeVolume)
             shapeInput = (input('Please enter "C" to calculate the volume of a cube, "P" for the volume of a pyramid, "E" for the volume of an ellipsoid, or "quit" to quit: ')).upper()
         elif shapeInput == 'P' :
-            pyramidVolume = pyramidVolume()
-            print(pyramidVolume)
-            pyramidVolumeList.append(pyramidVolume)
+            pyramidVolume()
             shapeInput = (input('Please enter "C" to calculate the volume of a cube, "P" for the volume of a pyramid, "E" for the volume of an ellipsoid, or "quit" to quit: ')).upper()
         elif shapeInput == 'E' :
-            ellipsoidVolume = ellipsoidVolume()
-            print(ellipsoidVolume)
-            ellipsoidVolumeList.append(ellipsoidVolume)
+            ellipsoidVolume()
             shapeInput = (input('Please enter "C" to calculate the volume of a cube, "P" for the volume of a pyramid, "E" for the volume of an ellipsoid, or "quit" to quit: ')).upper()
         elif shapeInput == 'QUIT' :
             valid = True
@@ -59,14 +60,11 @@ def main() :
             print()
             shapeInput = (input('Please enter "C" to calculate the volume of a cube, "P" for the volume of a pyramid, "E" for the volume of an ellipsoid, or "quit" to quit: ')).upper()
     if len(cubeVolumeList) != 0 or len(pyramidVolumeList) != 0 or len(ellipsoidVolumeList) != 0 :
-        cubeVolumeList.sort()
-        pyramidVolumeList.sort()
-        ellipsoidVolumeList.sort()
         print()
         print('You have come to the end of the session.')
         print('The volumes calculated for each shape are shown below')
-        for i in range(len(cubeVolumeList)) :
-        print('Cube: {}'.format(cubeVolumeList))
+
+        print('Cube: ', cubeVolumeList)
         print('Pyramid:', pyramidVolumeList)
         print('Ellipsoid:', ellipsoidVolumeList)
     else :
