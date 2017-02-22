ec9ba88e299a97cd72674523d1d963ce1588a5e0
jblock3 <block.jamie123@gmail.com>
2017-02-21 20:53:42 -0500

    Final touches

--- jblock3_Assign2.py
+++ jblock3_Assign2.py
@@ -1,7 +1,7 @@
 ##
 # This program was coded by Jamie Block (Student number: 250777666)
 # The program is designed to calculate the volume of a cube, a pyramid, or an ellipsoid
-# as many times as he/she pleases.
+# as many times as the user pleases.
 # The sentinel value to end the loop and display the final output is "quit".
 #
 
@@ -10,7 +10,7 @@
 def cubeVolume() :
     sideLength = float(input('What is the length of one side of the cube?: '))
     volume = sideLength ** 3
-    cubeVolumeList.append(round(volume, 1))
+    cubeVolumeList.append(round(volume, 1))     # Adds the rounded volume to the given list
     cubeVolumeList.sort()
     print('\nThe volume of a pyramid with a side length of %f is %.1f' % (sideLength, round(volume, 1)))
 
@@ -24,7 +24,7 @@ def pyramidVolume() :
 
 def ellipsoidVolume() :
     from math import pi
-    r1 = float(input('What is the value of radius 1?: '))
+    r1 = float(input('What is the value of radius 1?: '))       # r1: radius 1, r2: radius 2, etc.
     r2 = float(input('What is the value of radius 2?: '))
     r3 = float(input('What is the value of radius 3?: '))
     volume = (4 / 3) * pi * r1 * r2 * r3
@@ -42,6 +42,9 @@ ellipsoidVolumeList = []
 
 def main() :
     shapeInput = (input('Please enter "C" to calculate the volume of a cube, "P" for the volume of a pyramid, "E" for the volume of an ellipsoid, or "quit" to quit: ')).upper()
+
+    # Creation of validity loop
+
     valid = False
     while not valid :
         if shapeInput == 'C' :
@@ -62,6 +65,9 @@ def main() :
             print('\nInvalid input! Please try again.')
             print()
             shapeInput = (input('Please enter "C" to calculate the volume of a cube, "P" for the volume of a pyramid, "E" for the volume of an ellipsoid, or "quit" to quit: ')).upper()
+
+    # Output after user enters "quit"
+
     if len(cubeVolumeList) != 0 or len(pyramidVolumeList) != 0 or len(ellipsoidVolumeList) != 0 :
         print()
         print('You have come to the end of the session.')
@@ -92,4 +98,4 @@ def main() :
         print('\nYou have come to the end of the session.')
         print('You did not perform any volume calculations')
 
-main()
+main()  # Calling the main function
